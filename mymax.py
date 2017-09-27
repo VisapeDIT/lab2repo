f3b0b7551ea7e0a84753d795b6ae0efa442637a5
visapeDIT <d17123420@mydit.ie>
2017-09-27 10:59:45 +0000

    A new code added

--- mymax.py
+++ mymax.py
@@ -1,1 +1,6 @@
-print "The max of an array"
+array = list()
+num = raw_input("Enter how many elements you want:")
+for i in range(int(num)):
+	n = raw_input("num: ")
+	array.append(int(n))
+print "The max value of the array is ", max(array)
